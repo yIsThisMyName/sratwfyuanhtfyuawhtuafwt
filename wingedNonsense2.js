a4f6ac03b04b573c510911b35a8193b3e25a1cfb
y <luw686021@gmail.com>
2026-03-21 13:14:53 -0700

    Add files via upload

---
+++ wingedNonsense2.js
@@ -0,0 +1,24 @@
+window.wingedNonsense2 = {};
+window.wingedNonsense2.runCodeBefore = function() {
+  
+}
+window.wingedNonsense2.alterSnakeCode = function(code) {
+  // messes with the direction of winged apples
+  code = assertReplace(
+    code,
+    /for\s*\(\s*const\s+a\s+of\s+this\.oa\s*\)\s*a\.Oo\s*\|\|\s*\(\s*a\.yL\s*=\s*\(\s*a\.yL\s*\+\s*1\s*\)\s*%\s*6\s*,\s*O6\s*\(\s*this\.settings\s*\)\s*&&\s*\(\s*a\.pL\.x\s*&&\s*\(\s*a\.pos\.x\s*\+=\s*a\.we\.x\s*\)\s*,\s*a\.pL\.y\s*&&\s*\(\s*a\.pos\.y\s*\+=\s*a\.we\.y\s*\)\s*,\s*P6\s*\(\s*this\.settings\s*,\s*4\s*\)\s*&&\s*\(\s*a\.pos\.x\s*<\s*0\s*\?\s*a\.pos\.x\s*\+=\s*this\.Aa\.Aa\.width\s*:\s*a\.pos\.x\s*>=\s*this\.Aa\.Aa\.width\s*&&\s*\(\s*a\.pos\.x\s*-\=\s*this\.Aa\.Aa\.width\s*\)\s*,\s*a\.pos\.y\s*<\s*0\s*\?\s*a\.pos\.y\s*\+=\s*this\.Aa\.Aa\.height\s*:\s*a\.pos\.y\s*>=\s*this\.Aa\.Aa\.height\s*&&\s*\(\s*a\.pos\.y\s*-\=\s*this\.Aa\.Aa\.height\s*\)\s*\)\s*\)\s*\)/,
+    "for (const a of this.oa){if(!Object.hasOwn(a, \"preX\"))a.preX=a.we.x;if(!Object.hasOwn(a, \"preY\"))a.preY=a.we.y;a.Oo || (a.yL = (a.yL + 1) % 6,O6(this.settings) && (a.pL.x && (a.pos.x += a.we.x),a.pL.y && (a.pos.y += a.we.y),P6(this.settings, 4) && (a.pos.x < 0 ? a.pos.x += this.Aa.Aa.width : a.pos.x >= this.Aa.Aa.width && (a.pos.x -= this.Aa.Aa.width),a.pos.y < 0 ? a.pos.y += this.Aa.Aa.height : a.pos.y >= this.Aa.Aa.height && (a.pos.y -= this.Aa.Aa.height))))if(a.we.x!=a.preX){a.we.x*=-1;a.preX=a.we.x}if(a.we.y!=a.preY){a.we.y*=-1;a.preY=a.we.y}}"
+  );
+  // for random direction changes every 10 ticks
+  /*if(!Object.hasOwn(a, \"wingedTimer\"))a.wingedTimer=0;a.wingedTimer++;if(a.wingedTimer%10==0){a.we.x=((Math.floor(Math.random()*3)-1)/2);a.we.y=((Math.floor(Math.random()*3)-1)/2);console.log(a.we.y);}*/
+  // random speed changes every tick
+  /*a.we.x+=(Math.floor(Math.random()*3)-1)/10;a.we.y+=(Math.floor(Math.random()*3)-1)/10;*/
+  // double the speed
+  /*if(!(a.we.x%1==0))a.we.x*=2;if(!(a.we.y%1==0))a.we.y*=2;*/
+  // nobody likes isak
+  /*if(!Object.hasOwn(a, \"preX\"))a.preX=a.we.x;if(!Object.hasOwn(a, \"preY\"))a.preY=a.we.y;     code stuff here for bouncing apples       if(a.we.x!=a.preX){a.we.x*=-1;a.preX=a.we.x}if(a.we.y!=a.preY){a.we.y*=-1;a.preY=a.we.y}*/
+  return code;
+}
+window.wingedNonsense2.runCodeAfter = function() {
+  
+}
